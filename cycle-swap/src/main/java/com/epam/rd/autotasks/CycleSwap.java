fc8e84a70d32d0924d8c51aa7ecf3ab889e38fbc
Prateek Singh <94031538+Prateeksingh25@users.noreply.github.com>
2023-02-16 11:33:46 +0530

    Add files via upload

---
+++ cycle-swap/src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -0,0 +1,29 @@
+package com.epam.rd.autotasks;
+
+class CycleSwap {
+    static void cycleSwap(int[] array) {
+        if(array.length==0){
+            return;
+        }
+        else{
+            int x = array[array.length-1], i;
+            for (i = array.length-1; i > 0; i--)
+                array[i] = array[i-1];
+            array[0] = x;}
+
+    }
+
+    static void cycleSwap(int[] array, int shift) {
+        if(array.length==0 || shift==0){
+            return;
+        }
+        else{
+            for(int j=0;j<shift;j++) {
+                int x = array[array.length - 1], i;
+                for (i = array.length - 1; i > 0; i--)
+                    array[i] = array[i - 1];
+                array[0] = x;
+            }}
+        //System.out.print(shift);
+    }
+}
